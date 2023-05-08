20277dbc4fea2b7dd426f2f77f771d880a924a89
yaw.o.k <obengky72@gmail.com>
2023-05-08 10:55:54 +0000

    Add split length finder

--- video_splitter.py
+++ video_splitter.py
@@ -1,4 +1,4 @@
-from ffmpeg_split import split_by_seconds
+from ffmpeg_split import split_by_seconds, get_video_length
 import os
 import settings
 
@@ -8,23 +8,29 @@ class Video_splitter:
 
     def __init__(self):
         self.seconds = settings.SPLIT_SIZE
+        self.split_by_number = False 
+        self.split_number = None
 
     def change_seconds(self, new_seconds):
         self.seconds = new_seconds
+        
+    
+    def find_split_length(filename, split_number):
+        video_length = get_video_length(filename)
+        return video_length / split_number
+        
 
         
     def split(self, filename, seconds=None):
         if seconds:
             video_files = split_by_seconds(filename, seconds, "h264")
+        elif self.split_by_number:
+            video_files = split_by_seconds(filename, self.split_number, "h264")
         else:
             video_files = split_by_seconds(filename, self.seconds, "h264")
 
         return video_files
 
-
-    def split_by_number(self, filename, video_length, number_of_splits):
-        seconds = (video_length * 60) / number_of_splits
-        self.split(filename, seconds)
 
         
     def remove(self, files):
