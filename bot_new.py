9c61b16fba157f276909670bca25fc9a6851253b
yaw.o.k <obengky72@gmail.com>
2023-05-15 00:04:41 +0000

    Add inline keyboard to select split type

--- bot_new.py
+++ bot_new.py
@@ -1,6 +1,13 @@
 import logging
 import os
-from telegram import Update, error
+from telegram import (
+    Update,
+    error,
+    ReplyKeyboardMarkup as RKM,
+    ReplyKeyboardRemove as RKR,
+    InlineKeyboardButton as IKB,
+    InlineKeyboardMarkup as IKM,
+)
 from telegram.ext import (
     ApplicationBuilder,
     ContextTypes,
@@ -16,28 +23,27 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-START, VIDEO, SPLIT_BY_SECOND, SPLIT_BY_PART = range(4)
+START, VIDEO, SECONDS_OR_PARTS, SPLIT = range(4)
 
-class Katana():
-    
-    def __init__(self, update, context):
+
+class Katana:
+    def __init__(self, update, context, rmk):
         self.context = context
         self.update = update
+        self.rmk = rmk
         self.user = None
         self.video_name
-    
-    
+
     async def start(self):
         """Start bot"""
         self.user = self.update.message.from_user
         logger.info("%s started the bot", self.user.first_name.title())
         await self.update.message.reply_text(
-            "I am Video Splitter. Send a video. Split size is 30 seconds."
-        ) 
-        
+            "I am Katana. Send a video you'd like to split"
+        )
+
         return VIDEO
-    
-    
+
     async def collect_video(self):
         try:
             os.mkdir(f"vid/{self.user.id}")
@@ -45,7 +51,21 @@ class Katana():
             logger.info("vid folder already exists.")
         file_id = self.update.message.video.file_id
         new_file = await self.context.bot.get_file(file_id)
-        self.video_name = await new_file.download_to_drive(f"vid/{self.user.id}/video.mp4")
-        logger.info("Saved %s ", self.video_name) 
-            
-        
+        self.video_name = await new_file.download_to_drive(
+            f"vid/{self.user.id}/video.mp4"
+        )
+        logger.info("Saved %s ", self.video_name)
+
+        reply_keyboard = [["Seconds"], ["Parts"]]
+        self.update.message.reply_text(
+            """Do you want to split the video by seconds or by parts?""",
+            reply_markup=self.rmk(
+                reply_keyboard,
+                one_time_keyboard=True,
+            ),
+        )
+
+        return SECONDS_OR_PARTS
+
+
+
