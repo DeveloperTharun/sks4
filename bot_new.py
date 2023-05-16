bf765aad19ca94b0d4c57182012feef6bcc374d9
yaw.o.k <obengky72@gmail.com>
2023-05-16 23:47:52 +0000

    Add split_video function

--- bot_new.py
+++ bot_new.py
@@ -30,16 +30,20 @@ class Katana:
     def __init__(self, update, context, rmk):
         self.context = context
         self.update = update
+        self.seconds = 0
+        self.parts = 0
+        self.rmk_input = None
         self.rmk = rmk
         self.user = None
         self.video_name
+        self.vs = Video_splitter()
 
     async def start(self):
         """Start bot"""
         self.user = self.update.message.from_user
         logger.info("%s started the bot", self.user.first_name.title())
         await self.update.message.reply_text(
-            "I am Katana. Send a video you'd like to split"
+            "Send a video you'd like to split"
         )
 
         return VIDEO
@@ -67,5 +71,92 @@ class Katana:
 
         return SECONDS_OR_PARTS
 
+    async def collect_seconds(self):
+        self.rmk_input = self.update.message.text
+        logger.info(f"User chose to split video by seconds")
+        await self.update.message.reply_text(
+            "Enter seconds"
+        )
 
+        return SPLIT
 
+    async def collect_parts(self):
+        self.rmk_input = self.update.message.text
+        logger.info(f"User chose to split video by seconds")
+        await self.update.message.reply_text(
+            "Enter number of parts"
+        )
+
+        return SPLIT
+
+    async def split_video(self):
+        user_input = int(self.update.message.text)
+        if self.rmk_input == "Parts":
+            self.seconds = self.vs.find_split_length(user_input)
+            
+        # Download file.
+        sticker = await self.context.bot.send_sticker(
+            chat_id=self.update.effective_chat.id, sticker="loading.tgs"
+        )
+        sent_videos = []
+        try:
+            # split
+            split_videos = self.vs.split(str(self.video_name), self.seconds)
+            for v in split_videos:
+                sent_videos.append(v)
+                await self.context.bot.send_video(chat_id=self.update.effective_chat.id, video=v)
+
+                # Remove files to reuse folder
+            self.vs.remove(split_videos)
+            self.vs.remove([str(self.video_name)])
+            logger.info("Removed %s and split videos", self.video_name)
+            await self.context.bot.delete_message(
+                chat_id=self.update.effective_chat.id, message_id=sticker.id
+            )
+
+        except error.BadRequest:
+            video_size = self.update.message.video.file_size
+            await self.update.message.reply_text(
+                f"""
+            Size of video too large to be saved. Please
+            try again with a smaller video size.
+            Current video size: {int(video_size/1000000)}mb.
+            Bot filesize limit: 20mb.
+            """,
+            )
+            logger.info("Video too large; ask your to resend smaller video.")
+            await self.context.bot.delete_message(
+                chat_id=self.update.effective_chat.id, message_id=sticker.id
+            )
+        except SystemExit:
+            video_duration = self.update.message.video.duration
+            message = f"""
+            Video duration is too short to be split.
+            Current video duration: {video_duration} seconds.
+            Current split size: {self.seconds} seconds.
+            You can use /split_size {video_duration/2} to split video into 2.
+            """
+            await self.update.message.reply_text(message)
+            self.vs.remove([str(self.video_name)])
+            logger.info("Removed %s.", self.video_name)
+            logger.info("Ask user to change split size to suit video duration.")
+            await self.context.bot.delete_message(
+                chat_id=self.update.effective_chat.id, message_id=sticker.id
+            )
+        except error.TimedOut:
+            for v in split_videos:
+                if v not in sent_videos:
+                    sent_videos.append(v)
+                    await self.context.bot.send_video(chat_id=self.update.effective_chat.id, video=v)
+
+            # Remove files to reuse folder
+            self.vs.remove(split_videos)
+            self.vs.remove([str(self.video_name)])
+            logger.info("Removed %s and split videos", self.video_name)
+            await self.context.bot.delete_message(
+                chat_id=self.update.effective_chat.id, message_id=sticker.id
+            )
+            
+        return START
+    
+    
